3ef3c8d16b218effbdbb36fb263ee97ae4a60142
Jacob Bickus <j.bickus2019@gmail.com>
2020-09-17 16:24:56 -0400

    removed cut for gamma

--- src/physicsList.cc
+++ src/physicsList.cc
@@ -35,80 +35,81 @@
 
 
 physicsList::physicsList(G4bool addNRF_in, G4bool use_xsec_tables_in, G4bool use_xsec_integration_in, G4bool force_isotropic_in)
-  : addNRF(addNRF_in),
-    use_xsec_tables(use_xsec_tables_in),
-    use_xsec_integration(use_xsec_integration_in),
-    force_isotropic(force_isotropic_in) {
-  defaultCutValue = 0.05*mm;
-  cutForGamma = defaultCutValue;
-  cutForElectron = defaultCutValue;
-  cutForPositron = defaultCutValue;
-  cutForProton = defaultCutValue;
+        : addNRF(addNRF_in),
+        use_xsec_tables(use_xsec_tables_in),
+        use_xsec_integration(use_xsec_integration_in),
+        force_isotropic(force_isotropic_in) {
+        defaultCutValue = 0.05*mm;
+        //cutForGamma = defaultCutValue;
+        cutForElectron = defaultCutValue;
+        cutForPositron = defaultCutValue;
+        cutForProton = defaultCutValue;
 
-  G4HadronicProcessStore::Instance()->SetVerbose(0);
+        G4HadronicProcessStore::Instance()->SetVerbose(0);
 
-  ConstructPhysics();
+        ConstructPhysics();
 }
 
 
 physicsList::~physicsList()
-{;}
+{
+}
 
 void physicsList::ConstructPhysics() {
-  G4DecayPhysics *theDecayPhysics = new G4DecayPhysics();
-  theDecayPhysics->ConstructParticle();
-  RegisterPhysics(theDecayPhysics);
+        G4DecayPhysics *theDecayPhysics = new G4DecayPhysics();
+        theDecayPhysics->ConstructParticle();
+        RegisterPhysics(theDecayPhysics);
 
-  // Add OpticalPhysics to physicsList
-  G4OpticalPhysics* opticalPhysics = new G4OpticalPhysics(0);
-  opticalPhysics->SetWLSTimeProfile("delta");
-  opticalPhysics->SetScintillationYieldFactor(1.0);
-  opticalPhysics->SetScintillationExcitationRatio(0.0);
-  G4int maxNumber = 500;
-  opticalPhysics->SetMaxNumPhotonsPerStep(maxNumber);
-  opticalPhysics->SetMaxBetaChangePerStep(10.0);
-  opticalPhysics->SetTrackSecondariesFirst(kCerenkov, true);
-  opticalPhysics->SetTrackSecondariesFirst(kScintillation, true);
-  RegisterPhysics(opticalPhysics);
+        // Add OpticalPhysics to physicsList
+        G4OpticalPhysics* opticalPhysics = new G4OpticalPhysics(0);
+        opticalPhysics->SetWLSTimeProfile("delta");
+        opticalPhysics->SetScintillationYieldFactor(1.0);
+        opticalPhysics->SetScintillationExcitationRatio(0.0);
+        G4int maxNumber = 500;
+        opticalPhysics->SetMaxNumPhotonsPerStep(maxNumber);
+        opticalPhysics->SetMaxBetaChangePerStep(10.0);
+        opticalPhysics->SetTrackSecondariesFirst(kCerenkov, true);
+        opticalPhysics->SetTrackSecondariesFirst(kScintillation, true);
+        RegisterPhysics(opticalPhysics);
 
-  // Add NRF to the physicsList
-  if(addNRF)
-  {
-    RegisterPhysics(new G4NRFPhysics("NRF", use_xsec_tables, use_xsec_integration, force_isotropic));
-    G4cout << "\nAdded NRF to the physicsList.\n" << G4endl;
-    std::cout << "Added NRF to the PhysicsList." << std::endl;
-  }
+        // Add NRF to the physicsList
+        if(addNRF)
+        {
+                RegisterPhysics(new G4NRFPhysics("NRF", use_xsec_tables, use_xsec_integration, force_isotropic));
+                G4cout << "\nAdded NRF to the physicsList.\n" << G4endl;
+                std::cout << "Added NRF to the PhysicsList." << std::endl;
+        }
 
-  // Add the rest of the usual suspects
-  RegisterPhysics(new G4EmStandardPhysics_option4(0));
-  RegisterPhysics(new G4EmExtraPhysics(0));
+        // Add the rest of the usual suspects
+        RegisterPhysics(new G4EmStandardPhysics_option4(0));
+        RegisterPhysics(new G4EmExtraPhysics(0));
 
-  // Ion stopping-in-matter physics
-  RegisterPhysics( new G4StoppingPhysics(0) );
+        // Ion stopping-in-matter physics
+        RegisterPhysics( new G4StoppingPhysics(0) );
 
-  // Neutron tracking cuts for optimized simulation
-  G4NeutronTrackingCut *theNeutronTrackingCut = new G4NeutronTrackingCut(0);
-  theNeutronTrackingCut->SetTimeLimit(10*microsecond);
-  theNeutronTrackingCut->SetKineticEnergyLimit(0.01*eV);
-  RegisterPhysics( theNeutronTrackingCut );
+        // Neutron tracking cuts for optimized simulation
+        G4NeutronTrackingCut *theNeutronTrackingCut = new G4NeutronTrackingCut(0);
+        theNeutronTrackingCut->SetTimeLimit(10*microsecond);
+        theNeutronTrackingCut->SetKineticEnergyLimit(0.01*eV);
+        RegisterPhysics( theNeutronTrackingCut );
 }
 
 
 void physicsList::ConstructParticle() {
-  G4Gamma::Definition();
-  G4Electron::Definition();
-  G4Positron::Definition();
+        G4Gamma::Definition();
+        G4Electron::Definition();
+        G4Positron::Definition();
 }
 
 
 void physicsList::ConstructProcess() {
-  G4VModularPhysicsList::ConstructProcess();
+        G4VModularPhysicsList::ConstructProcess();
 }
 
 
 void physicsList::SetCuts() {
-  SetCutValue(cutForGamma, "gamma");
-  SetCutValue(cutForElectron, "e-");
-  SetCutValue(cutForPositron, "e+");
-  SetCutValue(cutForProton, "proton");
+        //SetCutValue(cutForGamma, "gamma");
+        SetCutValue(cutForElectron, "e-");
+        SetCutValue(cutForPositron, "e+");
+        SetCutValue(cutForProton, "proton");
 }

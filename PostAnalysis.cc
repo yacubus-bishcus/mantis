761002ec64bae4cfdbb174acef82401cf1f8f2ee
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2020-11-18 20:57:29 -0500

    added t test

--- PostAnalysis.cc
+++ PostAnalysis.cc
@@ -9,18 +9,11 @@ void PostAnalysis(const char *ChopOn, const char *ChopOff)
     bool confirmation = chopOn->cd();
     double chopOn_entries;
     double chopOn_sum;
-    double chopOn_Energy_Sum_Guess;
-    double chopOnBinValue;
-    double chopOn_Energy_Counter = 0;
     double chopOn_inc_sum;
     
     double chopOff_entries;
     double chopOff_sum;
-    double chopOff_Energy_Sum_Guess;
-    double chopOffBinValue;
-    double chopOff_Energy_Counter = 0;
     double chopOff_inc_sum;
-    Int_t nbins;
     
     // Variables Set, Complete Calculations 
     
@@ -32,15 +25,8 @@ void PostAnalysis(const char *ChopOn, const char *ChopOff)
         chopOn->GetObject("IncWater",IncWater);
         
         chopOn_entries = Detected->GetEntries();
-        nbins = Detected->GetSize() - 2;
         chopOn_sum = Detected->Integral();
-        chopOn_Energy_Sum_Guess = chopOn_entries*Detected->GetMean();
         chopOn_inc_sum = IncWater->Integral();
-        for(Int_t i=0;i<nbins;i++)
-        {
-          chopOnBinValue = Detected->GetBinContent(i);
-          chopOn_Energy_Counter = chopOn_Energy_Counter + chopOnBinValue;
-        }
     }
     else
     {
@@ -55,18 +41,11 @@ void PostAnalysis(const char *ChopOn, const char *ChopOff)
         TH1D *IncWater2;
         chopOff->GetObject("Detected",Detected2);
         chopOff->GetObject("IncWater",IncWater2);
-        
-        nbins = Detected2->GetSize() - 2;
+
         chopOff_entries = Detected2->GetEntries();
         chopOff_sum = Detected2->Integral();
-        chopOff_Energy_Sum_Guess = chopOff_entries*Detected2->GetMean();
         chopOff_inc_sum = IncWater2->Integral();
         
-        for(Int_t i=0;i<nbins;i++)
-        {
-          chopOffBinValue = Detected2->GetBinContent(i);
-          chopOff_Energy_Counter = chopOff_Energy_Counter + chopOffBinValue;
-        }
     }
     else
     {
@@ -77,12 +56,10 @@ void PostAnalysis(const char *ChopOn, const char *ChopOff)
     std::cout << "Files Read Printing Results..." << std::endl;
     
     double per_diff = ((chopOn_sum - chopOff_sum)/chopOn_sum)*100.;
+    double t_test = abs(chopOn_sum - chopOff_sum)/sqrt(pow(chopOn_sum,2) + pow(chopOff_sum,2));
     std::cout << "On Entries: " << chopOn_entries << " On Sum: " << chopOn_sum << " On NRF: " << chopOn_inc_sum << std::endl;
     std::cout << "Off Entries: " << chopOff_entries << " Off Sum: " << chopOff_sum << " Off NRF: " << chopOff_inc_sum << std::endl;
     std::cout << "Weighted Sum Percent Difference: " << per_diff << " %" << std::endl;
-    std::cout << "Estimated Chopper On Total Energy Deposited: " << chopOn_Energy_Sum_Guess << std::endl;
-    std::cout << "Estimated Chopper Off Total Energy Deposited: " << chopOff_Energy_Sum_Guess << std::endl;
-    std::cout << "Summing Method Chopper On Total Energy Deposited: " << chopOn_Energy_Counter << std::endl;
-    std::cout << "Summing Method Chopper Off Total Energy Deposited: " << chopOff_Energy_Counter << std::endl;
+    std::cout << "T-test result: " << t_test << std::endl;
     
 }

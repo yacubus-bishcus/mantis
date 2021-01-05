2fe5980f38e72a72e20f4d865c0e576624ac729c
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-01-05 16:45:38 -0500

    Update DetectorMessenger.hh

--- include/DetectorMessenger.hh
+++ include/DetectorMessenger.hh
@@ -53,6 +53,7 @@ private:
   G4UIcmdWithADouble* CmdPlexi;
   G4UIcmdWithADouble* CmdTape;
   G4UIcmdWithAString* CmdVis;
+  G4UIcmdWithAString* CmdVerbose;
   G4UIdirectory *myDir;
   G4UIdirectory *myDir2;
   G4UIdirectory *myDir3;

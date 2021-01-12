0234fa0127ba5b3a72708b54111356467d99785f
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-01-11 21:12:57 -0500

    Update PostAnalysis.cc

--- PostAnalysis.cc
+++ PostAnalysis.cc
@@ -9,10 +9,10 @@ void PostAnalysis(const char *ChopOn, const char *ChopOff)
     TFile *chopOff = new TFile(ChopOff);
     bool confirmation = chopOn->cd();
     double chopOn_entries, intObjOn_entries, chopOn_sum, intObjOn_sum, detectedOn_entries, detectedOn_sum;
-    double chopOn_inc_sum = 0;
+    double chopOn_NRF_inc_sum = 0;
     
     double chopOff_entries, intObjOff_entries, chopOff_sum, intObjOff_sum, detectedOff_entries, detectedOff_sum;
-    double chopOff_inc_sum = 0;
+    double chopOff_NRF_inc_sum = 0;
     
     // Variables Set, Complete Calculations 
     
@@ -33,20 +33,21 @@ void PostAnalysis(const char *ChopOn, const char *ChopOff)
             {
                 throw response;
             }
-            else chopOn_inc_sum = NRFIncWater->Integral();
+            else chopOn_NRF_inc_sum = NRFIncWater->Integral();
         }
         catch(string response)
         {
             std::cout << response << std::endl;
             chopOn->GetObject("IncWater",IncWater);
-            chopOn_inc_sum = IncWater->Integral();
+            chopOn_NRF_inc_sum = IncWater->Integral();
             std::cout << "Older Chopper On File Processed." << std::endl;
         }
             
         chopOn_entries = IncChopper->GetEntries();
         intObjOn_entries = IntObj->GetEntries();
         detectedOn_entries = Detected->GetEntries();
-        chopOn_sum = IncChopper->Integral();
+        IncChopper->Draw("ChopperData>>chopHist", "", "goff");
+        chopOn_sum = chopHist->Integral();
         intObjOn_sum = IntObj->Integral();
         detectedOn_sum = Detected->Integral();
     }
@@ -74,20 +75,21 @@ void PostAnalysis(const char *ChopOn, const char *ChopOff)
             {
                 throw response;
             }
-            else chopOff_inc_sum = NRFIncWater2->Integral();
+            else chopOff_NRF_inc_sum = NRFIncWater2->Integral();
         }
         catch(string response)
         {
             std::cout << response << std::endl;
             chopOff->GetObject("IncWater",IncWater2);
-            chopOff_inc_sum = IncWater2->Integral();
+            chopOff_NRF_inc_sum = IncWater2->Integral();
             std::cout << "Older Chopper Off File Processed." << std::endl;
         }
             
         chopOff_entries = IncChopper2->GetEntries();
         intObjOff_entries = IntObj2->GetEntries();
         detectedOff_entries = Detected2->GetEntries();
-        chopOff_sum = IncChopper2->Integral();
+        IncChopper2->Draw("ChopperData>>chopHist2", "", "goff");
+        chopOff_sum = chopHist2->Integral();
         intObjOff_sum = IntObj2->Integral();
         detectedOff_sum = Detected2->Integral();
         
@@ -100,13 +102,19 @@ void PostAnalysis(const char *ChopOn, const char *ChopOff)
     
     std::cout << "Files Read. Printing Results..." << std::endl;
     
+    double chopper_z = abs(chopOn_sum - chopOff_sum)/(sqrt(pow(sqrt(chopOn_sum),2) + pow(sqrt(chopOff_sum),2)));
     double intObj_z = abs(intObjOn_sum - intObjOff_sum)/(sqrt(pow(sqrt(intObjOn_sum),2) + pow(sqrt(intObjOff_sum),2)));
     double detected_z = abs(chopOn_sum - chopOff_sum)/(sqrt(pow(sqrt(chopOn_sum),2) + pow(sqrt(chopOff_sum),2)));
+    
+    std::cout << "Chopper On Incident Chopper Wheel Entries: " << chopOn_entries << " On Sum: " << chopOn_sum << std::endl;
+    std::cout << "Chopper Off Incident Chopper Wheel Entries: " << chopOff_entries << " Off Sum: " << chopOff_sum << std::endl;
+    std::cout << "Chopper Wheel Z-test result: " << chopper_z << std::endl;
     
     std::cout << "Chopper On Interrogation Object Entries: " << intObjOn_entries << " On Sum: " << intObjOn_sum << std::endl;
     std::cout << "Chopper Off Interrogation Object Entries: " << intObjOff_entries << " Off Sum: " << intObjOff_sum << std::endl;
     std::cout << "Interrogation Object Z-test result: " << intObj_z << std::endl;
-    std::cout << "Chopper On Detected Entries: " << chopOn_entries << " On Sum: " << chopOn_sum << " On NRF: " << chopOn_inc_sum << std::endl;
-    std::cout << "Chopper Off Detected Entries: " << chopOff_entries << " Off Sum: " << chopOff_sum << " Off NRF: " << chopOff_inc_sum << std::endl;
+    
+    std::cout << "Chopper On Detected Entries: " << detectedOn_entries << " On Sum: " << detectedOn_sum << " On NRF: " << chopOn_NRF_inc_sum << std::endl;
+    std::cout << "Chopper Off Detected Entries: " << detectedOff_entries << " Off Sum: " << detectedOff_sum << " Off NRF: " << chopOff_NRF_inc_sum << std::endl;
     std::cout << "Detected Z-test result: " << detected_z << std::endl;
 }

c8e058b9c0ff8ae29cc6f55ccb08fcc5937362a8
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-01-12 14:46:38 -0500

    Update G4NRFPhysics.hh

--- include/G4NRFPhysics.hh
+++ include/G4NRFPhysics.hh
@@ -21,7 +21,7 @@
 
 class G4NRFPhysics : public G4VPhysicsConstructor {
  public:
-  G4NRFPhysics(const G4String &name, G4bool, G4bool, G4bool);
+  G4NRFPhysics(const G4String &name, G4bool, G4bool, G4bool, G4bool);
 
   virtual ~G4NRFPhysics();
 
@@ -32,6 +32,7 @@ class G4NRFPhysics : public G4VPhysicsConstructor {
   G4bool use_xsec_tables;
   G4bool use_xsec_integration;
   G4bool force_isotropic;
+  G4bool standalone;
 };
 
 #endif

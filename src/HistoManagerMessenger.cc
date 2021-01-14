8e745c727a253e2e4347272449db473cc2ceebae
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-01-14 10:34:39 -0500

    Create HistoManagerMessenger.cc

---
+++ src/HistoManagerMessenger.cc
@@ -0,0 +1,36 @@
+#include "HistoManagerMessenger.hh"
+
+
+PrimaryGenActionMessenger::PrimaryGenActionMessenger(PrimaryGeneratorAction* genAction)
+:genA(genAction)
+{
+  myDir = new G4UIdirectory("/input/");
+  myDir->SetGuidance("Input Commands");
+  Cmd = new G4UIcmdWithADouble("/input/energy",this);
+  Cmd->SetGuidance("Choose Desired Energy Input");
+  Cmd->SetParameterName("energy",false);
+  Cmd->SetDefaultValue(-1);
+
+}
+
+PrimaryGenActionMessenger::~PrimaryGenActionMessenger()
+{
+  delete Cmd;
+}
+
+
+void PrimaryGenActionMessenger::SetNewValue(G4UIcommand* command, G4String newValue)
+{ // this function only works if named SetNewValue
+
+  if(command == Cmd)
+  {
+    G4double theCommand = Cmd->GetNewDoubleValue(newValue);
+    std::cout << "Input Energy for Brem Test set to: " << theCommand << " MeV" << std::endl;
+    genA->SetEnergyValue(theCommand);
+
+  }
+  else
+  {
+    G4cerr << "ERROR PrimaryGenActionMessenger :: SetStepInputValue command != Cmd" << G4endl;
+  }
+}

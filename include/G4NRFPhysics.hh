01788cf7de9e0101fd79a98bec0cfac543806d10
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-01-14 17:05:41 -0500

    Update G4NRFPhysics.hh

--- include/G4NRFPhysics.hh
+++ include/G4NRFPhysics.hh
@@ -21,7 +21,7 @@
 
 class G4NRFPhysics : public G4VPhysicsConstructor {
  public:
-  G4NRFPhysics(const G4String &name, G4bool, G4bool, G4bool, G4bool);
+  G4NRFPhysics(const G4String &name, G4bool, G4bool, G4bool, G4bool, G4bool);
 
   virtual ~G4NRFPhysics();
 

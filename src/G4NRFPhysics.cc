12fa54ea7d3305f6324a7e37e4b9ba76173d3a26
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-01-14 17:04:27 -0500

    Update G4NRFPhysics.cc

--- src/G4NRFPhysics.cc
+++ src/G4NRFPhysics.cc
@@ -1,18 +1,15 @@
-// C++ source file to encapsulate the G4NRF physics process
-// Jayson Vavrek, MIT, 2016
-// jvavrek@mit.edu
-
 #include "G4NRFPhysics.hh"
 
 #include "G4RayleighScattering.hh"
 #include "G4LivermoreRayleighModel.hh"
 
 G4NRFPhysics::G4NRFPhysics(const G4String &name, G4bool use_xsec_tables_in,
-  G4bool use_xsec_integration_in, G4bool force_isotropic_in, G4bool standalone_in)
+  G4bool use_xsec_integration_in, G4bool force_isotropic_in, G4bool standalone_in, G4bool Verbose_in)
   : use_xsec_tables(use_xsec_tables_in),
     use_xsec_integration(use_xsec_integration_in),
     force_isotropic(force_isotropic_in),
-    standalone(standalone_in)
+    standalone(standalone_in),
+    Verbose(Verbose_in)
 {
   G4LossTableManager::Instance();
   const G4String theName = name;
@@ -26,7 +23,7 @@ void G4NRFPhysics::ConstructParticle()
 {}
 
 void G4NRFPhysics::ConstructProcess() {
-  G4NRF *nrf = new G4NRF("NRF", false, use_xsec_tables, use_xsec_integration, force_isotropic, standalone);
+  G4NRF *nrf = new G4NRF("NRF", Verbose, use_xsec_tables, use_xsec_integration, force_isotropic, standalone);
 
 
 // I edited this so that it works for Geant4 10.5

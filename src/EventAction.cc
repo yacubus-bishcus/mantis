22dae7b845e93b4223a5cad4a05e1facb64a6924
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-01-18 12:53:25 -0500

    Update EventAction.cc

--- src/EventAction.cc
+++ src/EventAction.cc
@@ -3,6 +3,8 @@
 EventAction::EventAction(): drawCherenkovDataFlag(0), eventM(NULL)
 {
   eventM = new EventMessenger(this);
+  eventInformation *anInfo = new eventInformation(anEvent);
+  G4AnalysisManager* manager = G4AnalysisManager::Instance();
 }
 
 EventAction::~EventAction()
@@ -21,9 +23,9 @@ void EventAction::EndOfEventAction(const G4Event* anEvent)
         tEvents = G4RunManager::GetRunManager()->GetCurrentRun()->GetNumberOfEventToBeProcessed();
         if(drawCherenkovDataFlag)
         {
-          G4AnalysisManager* manager = G4AnalysisManager::Instance();
-          eventInformation *anInfo = new eventInformation(anEvent);
           G4double beam_energy = anInfo->GetBeamEnergy();
+          std::cout << beam_energy << std::endl;
+          std::cout << cherenkov_counter << std::endl;
           manager->FillNtupleDColumn(1,0, beam_energy/(MeV));
           manager->FillNtupleIColumn(1,1, cherenkov_counter);
           manager->AddNtupleRow(1);

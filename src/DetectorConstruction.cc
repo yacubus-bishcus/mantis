dffa55e005e6dad528007d7b0dcbaad238e1af40
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-01-19 12:28:39 -0500

    Update DetectorConstruction.cc

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -127,6 +127,8 @@ if(bremTest)
 
 // Set up Collimator
 G4double container_z_pos = 2.4384*m +water_size_x + 1.0*m;
+G4RotationMatrix* collimatorRot = new G4RotationMatrix;
+collimatorRot->rotateY(90*deg);
 G4Box *solidCollimator = new G4Box("Collimator", 1*cm, water_size_y, container_z_pos - 2.4384*m);
 G4LogicalVolume * logicCollimator = new G4LogicalVolume(solidCollimator, lead, "Collimator");
 new G4PVPlacement(0, G4ThreeVector(-0.6096*m, 0, 0),
@@ -135,7 +137,7 @@ new G4PVPlacement(0, G4ThreeVector(-0.6096*m, 0, 0),
 new G4PVPlacement(0, G4ThreeVector(0.6096*m, 0, 0),
                   logicCollimator, "CollimatorRight", logicWorld,
                   false, 0, checkOverlaps);
-new G4PVPlacement(0, G4ThreeVector(0,0,-10*cm),
+new G4PVPlacement(collimatorRot, G4ThreeVector(0,0,-10*cm),
                   logicCollimator, "CollimatorRear", logicWorld,
                   false, 0, checkOverlaps);
 // Set up shipping container environment (8ft wide and 8.5ft high)

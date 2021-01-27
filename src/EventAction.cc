6b288f5fc879c7196d5bf89eb025139682c42598
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-01-27 14:13:22 -0500

    Update EventAction.cc

--- src/EventAction.cc
+++ src/EventAction.cc
@@ -1,40 +1,23 @@
 #include "EventAction.hh"
 
-EventAction::EventAction(): drawCherenkovDataFlag(0), eventM(NULL)
+EventAction::EventAction()
 {
-  eventM = new EventMessenger(this);
 }
 
 EventAction::~EventAction()
-{
-  delete eventM;
-}
-
+{}
 
 void EventAction::BeginOfEventAction(const G4Event*)
 {
-  cherenkov_counter = 0;
-  isNRF = 0;
 }
 
 void EventAction::EndOfEventAction(const G4Event* anEvent)
 {
-        tEvents = G4RunManager::GetRunManager()->GetCurrentRun()->GetNumberOfEventToBeProcessed();
-        eventInformation* info = (eventInformation*)(G4RunManager::GetRunManager()->GetCurrentEvent()->GetUserInformation());
-        if(drawCherenkovDataFlag && cherenkov_counter != 0)
-        {
-          G4AnalysisManager* manager = G4AnalysisManager::Instance();
-          G4double beam_energy = info->GetBeamEnergy();
-          G4int eventID = anEvent->GetEventID();
-          manager->FillNtupleDColumn(1,0, beam_energy/(MeV));
-          manager->FillNtupleIColumn(1,1, cherenkov_counter);
-          manager->FillNtupleIColumn(1,2, eventID);
-          manager->FillNtupleIColumn(1,3, isNRF);
-          manager->AddNtupleRow(1);
-        }
-        
-        if(anEvent->GetEventID() != 0 && anEvent->GetEventID() % 10000 == 0)
-        {
-          std::cout << "\r\tEvent & % Complete " << "\t" << anEvent->GetEventID() << "\t" << 100.*(anEvent->GetEventID()/(double)tEvents) << " %" << std::flush;
-        }
+  tEvents = G4RunManager::GetRunManager()->GetCurrentRun()->GetNumberOfEventToBeProcessed();
+
+  if(anEvent->GetEventID() != 0 && anEvent->GetEventID() % 10000 == 0)
+  {
+    std::cout << "\r\tEvent & % Complete " << "\t" << anEvent->GetEventID() 
+              << "\t" << 100.*(anEvent->GetEventID()/(double)tEvents) << " %" << std::flush;
+  }
 }

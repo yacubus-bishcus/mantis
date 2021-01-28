ba2a67b5be2c47728fdbd68a82819bea65c5c8f0
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-01-28 10:22:27 -0500

    Update Analysis.cc

--- Run_Analysis/Analysis.cc
+++ Run_Analysis/Analysis.cc
@@ -1,9 +1,34 @@
+//
+// ************************************************************************************************ // 
+// ************************************************************************************************ //
+// To Run:
+// root -b -q 'Analysis.cc("testOn","testOff", 2.1)'
+// ************************************************************************************************ //
+// ************************************************************************************************ //
+// File Explanation:
+// 
+// Execution of this script in ROOT CERN executes 4 other scripts in ROOT CERN 
+// 1. Cherenkov.cc
+// 2. EventCheck.cc
+// 3. WeightHisto.cc
+// 4. PrintResults.cc
+// 
+// Analysis.cc takes 3 inputs:
+// 1. Chopper On File root name (without .root extension)
+// 2. Chopper Off File root name (without .root extension)
+// 3. Max Energy of Bremsstrahlung Interrogation Beam 
+//
+// ************************************************************************************************ //
+// ************************************************************************************************ //
+
 #include "WeightHisto.cc"
 #include "EventCheck.cc"
 #include "Cherenkov.cc"
 #include "PrintResults.cc"
 
-void Analysis(const char* InputFilenameBaseOn, const char* InputFilenameBaseOff, const char* OutputFilenameBase, double Emax)
+void Analysis(const char* InputFilenameBaseOn, 
+              const char* InputFilenameBaseOff, 
+              const char* OutputFilenameBase, double Emax)
 {
     std::cout << "Merging Cherenkov, Checking Events, and Writing Weighted Histograms!" << std::endl;
     std::string inFileOn = InputFilenameBaseOn;

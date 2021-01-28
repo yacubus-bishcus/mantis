9bff5f08119113cf8ad4c1e9a034fd8e3566421a
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-01-28 09:34:09 -0500

    Update Analysis.cc

--- Run_Analysis/Analysis.cc
+++ Run_Analysis/Analysis.cc
@@ -5,7 +5,7 @@
 
 void Analysis(const char* InputFilenameBaseOn, const char* InputFilenameBaseOff, const char* OutputFilenameBase, double Emax)
 {
-    std::cout << "Weighting histograms, Checking Events, and Merging Cherenkov!" << std::endl;
+    std::cout << "Merging Cherenkov, Checking Events, and Writing Weighted Histograms!" << std::endl;
     std::string inFileOn = InputFilenameBaseOn;
     std::string inFileOff = InputFilenameBaseOff;
     inFileOn = inFileOn + ".root";
@@ -13,13 +13,13 @@ void Analysis(const char* InputFilenameBaseOn, const char* InputFilenameBaseOff,
     const char* InputFilenameOn = inFileOn.c_str();
     const char* InputFilenameOff = inFileOff.c_str();
     // Chopper On
-    WeightHisto(InputFilenameOn, OutputFilenameBase, Emax, true);
-    EventCheck(InputFilenameOn, OutputFilenameBase, true);
     Cherenkov(InputFilenameOn, OutputFilenameBase, Emax, true);
+    EventCheck(InputFilenameBaseOn, OutputFilenameBase, true);
+    WeightHisto(InputFilenameOn, OutputFilenameBase, Emax, true);
     // Chopper Off
-    WeightHisto(InputFilenameOff, OutputFilenameBase, Emax, false);
-    EventCheck(InputFilenameOff, OutputFilenameBase, false);
     Cherenkov(InputFilenameOff, OutputFilenameBase, Emax, false);
+    EventCheck(InputFilenameBaseOff, OutputFilenameBase, false);
+    WeightHisto(InputFilenameOff, OutputFilenameBase, Emax, false);
     
     PrintResults(InputFilenameBaseOn, InputFilenameBaseOff);
     std::cout << std::endl << "Analysis Complete" << std::endl;

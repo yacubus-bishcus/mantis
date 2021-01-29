49b29f2b75cc1a51f93d0e625bf9720972265bba
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-01-29 11:03:27 -0500

    Update Cherenkov.cc

--- Run_Analysis/Cherenkov.cc
+++ Run_Analysis/Cherenkov.cc
@@ -76,7 +76,6 @@ void Cherenkov(const char *InputFilenameBase, double Emax, bool ChopState)
     std::cout << "Merging Cherenkov with " << numEntries << " number of entries..." << std::endl;
     for(int i=0;i<numEntries;i++)
     {
-        std::cout << "\r Event Complete:\t" << i << std::flush;
         while(nSum < numEntries)
         {
             // clear holding vector/variables 

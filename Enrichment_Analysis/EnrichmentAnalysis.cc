3913d15d1edc95c98d65a6064f9ce1d33c086755
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-01-30 14:49:12 -0500

    Update EnrichmentAnalysis.cc

--- Enrichment_Analysis/EnrichmentAnalysis.cc
+++ Enrichment_Analysis/EnrichmentAnalysis.cc
@@ -1,3 +1,32 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Author:
+// Jacob E Bickus, 2021
+// MIT, NSE
+// jbickus@mit.edu
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+///////////////////////////////////////////////////////////////////////////////
+// Description:
+// c++ source file to run on Areg Danagoulian's grasshopper code .dat output files
+// This file collects the data printed to grasshopper's output for enriched and
+// depleted runs and compares the number of counts incident the detector using 
+// a Z-Score test 
+
 #include "EnrichmentAnalysis.hh"
 
 int main(int argc, char* argv[])

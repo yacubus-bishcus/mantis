5cd3a9e85d4b4bac24032d470dbf63660f6f98a6
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-01-30 14:33:01 -0500

    Update eventInformation.cc

--- src/eventInformation.cc
+++ src/eventInformation.cc
@@ -25,32 +25,36 @@
 #include "eventInformation.hh"
 #include "G4ios.hh"
 
-eventInformation::eventInformation() {
-        weight = 0.;
-        beamEnergy = 0.;
+eventInformation::eventInformation() 
+{
+  weight = 0.;
+  beamEnergy = 0.;
 }
 
-eventInformation::eventInformation(const G4Event* anEvent) {
-        weight = 0.; //aTrack->GetWeight();
-        beamEnergy = 0.;
+eventInformation::eventInformation(const G4Event* anEvent) 
+{
+  weight = 0.; //aTrack->GetWeight();
+  beamEnergy = 0.;
 }
 
-eventInformation::eventInformation(const eventInformation* anEventInfo) {
-        weight = anEventInfo->GetWeight();
-        beamEnergy = anEventInfo->GetBeamEnergy();
+eventInformation::eventInformation(const eventInformation* anEventInfo) 
+{
+  weight = anEventInfo->GetWeight();
+  beamEnergy = anEventInfo->GetBeamEnergy();
 }
 
-eventInformation::~eventInformation() {
-        ;
+eventInformation::~eventInformation() 
+{}
+
+void eventInformation::SetWeight(G4double x) 
+{
+  weight = x;
 }
 
-void eventInformation::SetWeight(G4double x) {
-        weight = x;
-}
-
-void eventInformation::SetBeamEnergy(G4double x) {
-        beamEnergy = x;
+void eventInformation::SetBeamEnergy(G4double x) 
+{
+  beamEnergy = x;
 }
 
 void eventInformation::Print() const
-{;}
+{}

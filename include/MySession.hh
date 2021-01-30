a633f199efb7608488cbdbd955b6e5f2ed3da2c9
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-01-30 14:24:57 -0500

    Update MySession.hh

--- include/MySession.hh
+++ include/MySession.hh
@@ -1,10 +1,32 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Author:
+// Jacob E Bickus, 2021
+// MIT, NSE
+// jbickus@mit.edu
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+///////////////////////////////////////////////////////////////////////////////
+
 #ifndef MySession_h
 #define MySession_h 1
 
 #include "G4UIsession.hh"
-//#include "G4ios.hh"
 #include <fstream>
-
 
 class MySession : public G4UIsession
 {

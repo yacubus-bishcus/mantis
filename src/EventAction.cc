25b1eb9d7abbd1627694efdeda5bcd1bf37997e6
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-02-02 10:08:34 -0500

    Update EventAction.cc

--- src/EventAction.cc
+++ src/EventAction.cc
@@ -67,7 +67,7 @@ void EventAction::EndOfEventAction(const G4Event* anEvent)
   
   tEvents = G4RunManager::GetRunManager()->GetCurrentRun()->GetNumberOfEventToBeProcessed();
 
-  if(anEvent->GetEventID() != 0 && anEvent->GetEventID() % 1000 == 0)
+  if(anEvent->GetEventID() != 0 && anEvent->GetEventID() % 100 == 0)
   {
     std::cout << "\r\tEvent & % Complete " << "\t" << anEvent->GetEventID() 
               << "\t" << 100.*(anEvent->GetEventID()/(double)tEvents) << " %" << std::flush;

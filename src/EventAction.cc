1f19e09be2cbf7801cf6effcf9a659538b9adb69
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-02-02 10:51:56 -0500

    Update EventAction.cc

--- src/EventAction.cc
+++ src/EventAction.cc
@@ -41,30 +41,7 @@ void EventAction::BeginOfEventAction(const G4Event*)
 void EventAction::EndOfEventAction(const G4Event* anEvent)
 {
   //std::cout << "EventAction::EndOfEventAction -> Beginning" << std::endl;
-  if(c_secondaries > 0)
-  {
-    // Grab Max Energy
-    //G4double maxE = *std::max_element(energyv.begin(),energyv.end());
-    G4double maxE = 1.0;
-    // Find Max Energy's Weight
-    eventInformation* info = (eventInformation*)(G4RunManager::GetRunManager()->GetCurrentEvent()->GetUserInformation());
-    weight = info->GetWeight();
-    // Find the Average Time 
-    if(timev.size() > 0)
-    {
-      c_time = calcAvg();
-    }
-    else
-      c_time = 0;
-    // Fill the TTree 
-    G4AnalysisManager* manager = G4AnalysisManager::Instance();
-    manager->FillNtupleDColumn(3,0,maxE);
-    manager->FillNtupleDColumn(3,1, weight);
-    manager->FillNtupleIColumn(3,2,anEvent->GetEventID());
-    manager->FillNtupleIColumn(3,3,c_secondaries);
-    manager->FillNtupleDColumn(3,4,c_time);
-    manager->AddNtupleRow(3);
-  }
+
   
   tEvents = G4RunManager::GetRunManager()->GetCurrentRun()->GetNumberOfEventToBeProcessed();
 

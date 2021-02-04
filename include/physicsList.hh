3dddd1753fc453ddbcb15ee55467d1d96d591dbc
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-02-04 10:40:29 -0500

    Update physicsList.hh

--- include/physicsList.hh
+++ include/physicsList.hh
@@ -26,6 +26,37 @@
 #define physicsList_hh 1
 
 #include "G4VModularPhysicsList.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4PhysicalConstants.hh"
+#include "G4DecayPhysics.hh"
+#include "G4NRFPhysics.hh"
+
+#include "G4OpticalPhysics.hh"
+#include "G4Cerenkov.hh"
+#include "G4OpAbsorption.hh"
+#include "G4OpRayleigh.hh"
+#include "G4OpMieHG.hh"
+
+#include "G4EmStandardPhysics_option4.hh"
+#include "G4EmExtraPhysics.hh"
+#include "G4EmProcessOptions.hh"
+#include "G4HadronPhysicsQGSP_BIC_HP.hh"
+#include "G4HadronPhysicsQGSP_BIC.hh"
+#include "G4HadronElasticPhysics.hh"
+#include "G4HadronElasticPhysicsHP.hh"
+#include "G4NeutronCrossSectionXS.hh"
+#include "G4NeutronTrackingCut.hh"
+#include "G4StoppingPhysics.hh"
+#include "G4HadronicProcessStore.hh"
+#include "G4LossTableManager.hh"
+#include "G4ProcessTable.hh"
+#include "G4ProcessManager.hh"
+#include "G4ProcessVector.hh"
+#include "G4ParticleTypes.hh"
+#include "G4ParticleTable.hh"
+#include "G4Gamma.hh"
+#include "G4Electron.hh"
+#include "G4Positron.hh"
 
 
 class physicsList: public G4VModularPhysicsList {

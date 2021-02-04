dd59072e1e03f7aef13346580f9fe4795d487a5b
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-02-03 23:18:28 -0500

    Update PrimaryGeneratorAction.cc

--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -84,12 +84,6 @@ PrimaryGeneratorAction::PrimaryGeneratorAction(G4bool bremTest, G4bool resonance
   }
 
 #endif
-  // Set Beam Position
-  beam_offset_x = 0*cm;
-  beam_offset_y = 0*cm;
-  z0 = 100*cm;
-  beam_size = 1.3*mm;
-  source_width=0; //by default the width along Z is zero
 
 }
 
@@ -132,22 +126,15 @@ void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 
 
   const float pi=acos(-1);
-
+  G4double beam_size = 1.3*mm;
   // Set beam position
-  r = beam_size*acos(G4UniformRand())/pi*2.;
-  ph = 360.*G4UniformRand()*CLHEP::deg;
-  x_r = r*cos(ph);
-  y_r = r*sin(ph);
-  z_r = source_width*(G4UniformRand()-0.5);
-  fParticleGun->SetParticlePosition(G4ThreeVector(x_r+beam_offset_x,y_r+beam_offset_y,z_r+z0));
+  G4double x_r = beam_size*acos(G4UniformRand())/pi*2.*cos(360.*G4UniformRand()*CLHEP::deg;);
+  G4double y_r = beam_size*acos(G4UniformRand())/pi*2.*sin(360.*G4UniformRand()*CLHEP::deg;);
+  fParticleGun->SetParticlePosition(G4ThreeVector(x_r,y_r,100*cm));
 
 
   // Set beam momentum
-  theta = 0.*CLHEP::deg;
-  phi = 0.*CLHEP::deg;
-  G4ThreeVector vDir;
-  vDir = G4ThreeVector(sin(theta)*cos(phi),sin(theta)*sin(phi),cos(theta));
-  fParticleGun->SetParticleMomentumDirection(vDir);
+  fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0,0,1)); // along z axis 
 
   fParticleGun->GeneratePrimaryVertex(anEvent);
   G4double w = 1.0;

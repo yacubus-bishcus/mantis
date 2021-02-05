a3156749a128332f8254062cc21e05ad049c7eb9
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-02-05 16:20:11 -0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -28,7 +28,8 @@ done
 echo "Geant4 Directory: $GEANT4_DIR"
 echo "ROOT Directory: $ROOT_DIRECTORY"
 
-if([$GEANT4_DIR = "None"])
+if[$GEANT4_DIR = "None"]
+then
     exit 0
 fi
 
@@ -44,7 +45,8 @@ database_working_dir="$(pwd)"
 echo "Exporting the Database working directory path: $database_working_dir"
 export G4NRFGAMMADATA=$database_working_dir
 
-if($ROOT_DIRECTORY != "None")
+if[$ROOT_DIRECTORY != "None"]
+then
     VARROOTBIN=$ROOT_DIRECTORY
 else
     VAR3="thisroot.sh"

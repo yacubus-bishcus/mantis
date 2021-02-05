3d4eab1c41705b45d8265267d77b65ae55f302ff
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-02-04 20:39:33 -0500

    update rayleigh and scintillation

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -55,7 +55,7 @@ G4Material *air = nist->FindOrBuildMaterial("G4_AIR");
 G4Material *steel = nist->FindOrBuildMaterial("G4_STAINLESS-STEEL");
 G4Material *poly = nist->FindOrBuildMaterial("G4_POLYETHYLENE");
 //G4Material *Water = nist->FindOrBuildMaterial("G4_WATER");
-// Need to have water built by user for raleigh scattering 
+// Need to have water built by user for raleigh scattering
 G4Material *tungsten = nist->FindOrBuildMaterial("G4_W");
 G4Material *lead = nist->FindOrBuildMaterial("G4_Pb");
 G4Material *gold = nist->FindOrBuildMaterial("G4_Au");
@@ -67,7 +67,7 @@ G4Element *elN = new G4Element("Nitrogen", "N2", 7, 14.01*g/mole);
 G4Element *elO = new G4Element("Oxygen", "O", 8, 16.0*g/mole);
 G4Element *elC = new G4Element("Carbon", "C", 6, 12.0*g/mole);
 G4Element *elH = new G4Element("Hydrogen", "H", 1, 1.00794*g/mole);
-G4Material *Water = new G4Material("Water", 0.99802*g/cm3,2, kStateLiquid,273.15, 1); // Name, density, numComponents, state, Temperature, pressure 
+G4Material *Water = new G4Material("Water", 0.99802*g/cm3,2, kStateLiquid,273.15, 1); // Name, density, numComponents, state, Temperature, pressure
 Water->AddElement(elH, 1);
 Water->AddElement(elO, 2);
 G4Material *plexiglass = nist->FindOrBuildMaterial("G4_PLEXIGLASS");
@@ -544,87 +544,87 @@ else
 
 // ************************** WATER ********************************//
         
-G4double photonEnergy[] =
-{ 2.034*eV, 2.068*eV, 2.103*eV, 2.139*eV,
-  2.177*eV, 2.216*eV, 2.256*eV, 2.298*eV,
-  2.341*eV, 2.386*eV, 2.433*eV, 2.481*eV,
-  2.532*eV, 2.585*eV, 2.640*eV, 2.697*eV,
-  2.757*eV, 2.820*eV, 2.885*eV, 2.954*eV,
-  3.026*eV, 3.102*eV, 3.181*eV, 3.265*eV,
-  3.353*eV, 3.446*eV, 3.545*eV, 3.649*eV,
-  3.760*eV, 3.877*eV, 4.002*eV, 4.136*eV };
+    G4double photonEnergyIOF[] =
+    { 2.034*eV, 2.068*eV, 2.103*eV, 2.139*eV,
+      2.177*eV, 2.216*eV, 2.256*eV, 2.298*eV,
+      2.341*eV, 2.386*eV, 2.433*eV, 2.481*eV,
+      2.532*eV, 2.585*eV, 2.640*eV, 2.697*eV,
+      2.757*eV, 2.820*eV, 2.885*eV, 2.954*eV,
+      3.026*eV, 3.102*eV, 3.181*eV, 3.265*eV,
+      3.353*eV, 3.446*eV, 3.545*eV, 3.649*eV,
+      3.760*eV, 3.877*eV, 4.002*eV, 4.136*eV
+    };
+    const G4int nEntriesIOF = sizeof(photonEnergyIOF)/sizeof(G4double);
 
-const G4int nEntries = sizeof(photonEnergy)/sizeof(G4double);
-
-G4double photonEnergy2[] =
-{1.72*eV, 1.746197183*eV, 1.771142857*eV, 1.796811594*eV,1.823235294*eV,
- 1.850447761*eV, 1.878484848*eV, 1.907384615*eV, 1.9371875*eV,
- 1.967936508*eV, 1.999677419*eV, 2.032459016*eV, 2.066333333*eV,
- 2.101355932*eV,2.137586207*eV, 2.175087719*eV, 2.213928571*eV, 2.254181818*eV,
- 2.295925926*eV, 2.339245283*eV, 2.384230769*eV, 2.430980392*eV, 2.4796*eV,
- 2.530204082*eV, 2.582916667*eV, 2.63787234*eV, 2.695217391*eV,
- 2.755111111*eV, 2.817727273*eV, 2.883255814*eV, 2.951904762*eV,
- 3.023902439*eV, 3.0995*eV, 3.178974359*eV, 3.262631579*eV,
- 3.542285714*eV, 4.132666667*eV, 4.508363636*eV, 4.9592*eV
-};
-
-// Water need to add rayleigh scattering! -- added automatically if material name = Water
+    G4double photonEnergyAbs[] =
+    {1.72*eV, 1.746197183*eV, 1.771142857*eV, 1.796811594*eV,1.823235294*eV,
+     1.850447761*eV, 1.878484848*eV, 1.907384615*eV, 1.9371875*eV,
+     1.967936508*eV, 1.999677419*eV, 2.032459016*eV, 2.066333333*eV,
+     2.101355932*eV,2.137586207*eV, 2.175087719*eV, 2.213928571*eV, 2.254181818*eV,
+     2.295925926*eV, 2.339245283*eV, 2.384230769*eV, 2.430980392*eV, 2.4796*eV,
+     2.530204082*eV, 2.582916667*eV, 2.63787234*eV, 2.695217391*eV,
+     2.755111111*eV, 2.817727273*eV, 2.883255814*eV, 2.951904762*eV,
+     3.023902439*eV, 3.0995*eV, 3.178974359*eV, 3.262631579*eV,
+     3.542285714*eV, 4.132666667*eV, 4.508363636*eV, 4.9592*eV
+    };
+    const G4int nEntriesAbs = sizeof(photonEnergyAbs)/sizeof(G4double);
+    
+    // rayleigh scattering! -- added automatically if material name = Water
         
-G4double refractiveIndex1[] =
-{ 1.3435, 1.344,  1.3445, 1.345,  1.3455,
-  1.346,  1.3465, 1.347,  1.3475, 1.348,
-  1.3485, 1.3492, 1.35,   1.3505, 1.351,
-  1.3518, 1.3522, 1.3530, 1.3535, 1.354,
-  1.3545, 1.355,  1.3555, 1.356,  1.3568,
-  1.3572, 1.358,  1.3585, 1.359,  1.3595,
-  1.36,   1.3608};
+    G4double refractiveIndex1[] =
+    { 1.3435, 1.344,  1.3445, 1.345,  1.3455,
+      1.346,  1.3465, 1.347,  1.3475, 1.348,
+      1.3485, 1.3492, 1.35,   1.3505, 1.351,
+      1.3518, 1.3522, 1.3530, 1.3535, 1.354,
+      1.3545, 1.355,  1.3555, 1.356,  1.3568,
+      1.3572, 1.358,  1.3585, 1.359,  1.3595,
+      1.36,   1.3608
+    };
+    assert(sizeof(refractiveIndex1) == sizeof(photonEnergyIOF));
 
-assert(sizeof(refractiveIndex1) == sizeof(photonEnergy));
+    G4double absorption[] =
+    {0.812347685*m, 1.209189843*m, 1.602564103*m, 1.937984496*m, 2.150537634*m,
+     2.277904328*m, 2.43902439*m, 2.941176471*m, 3.217503218*m, 3.429355281*m,
+     3.629764065*m, 3.78214826*m, 4.496402878*m, 7.4019245*m, 11.16071429*m, 14.38848921*m,
+     16.15508885*m, 17.69911504*m, 21.09704641*m, 23.04147465*m, 24.44987775*m,
+     30.76923077*m, 49.01960784*m,66.66666667*m,78.74015748*m,94.33962264*m,
+     102.145046*m, 108.4598698*m, 157.480315*m, 202.020202*m, 220.2643172*m,
+     211.4164905*m, 150.8295626*m, 117.5088132*m, 87.95074758*m,
+     60.60606061*m,18.11594203*m,11.79245283*m,9.652509653*m
+    };
+    assert(sizeof(absorption) == sizeof(photonEnergyAbs));
+    
+    // wavelenths 650, 600, 550, 500, 450, 400, 360 (approx peak), 350, 340, 300, 250
+    G4double photonEnergyScintillation[] =
+    {1.90744*eV, 2.0664*eV, 2.25425*eV, 2.4796*eV, 2.75520*eV,
+     3.0996*eV, 3.44400*eV, 3.542405*eV, 3.64659*eV, 4.132806*eV, 4.95936*eV
+    }
+    const G4int nEntriesScint = sizeof(photonEnergyScintillation)/sizeof(G4double);
+    
+    G4double scintilD[] =
+    { 0.007, 0.016, 0.028, 0.045, 0.25, 0.5, 0.95,
+      0.5, 0.006, 0.001, 0.0001
+    };
 
-G4double absorption[] =
-{0.812347685*m, 1.209189843*m, 1.602564103*m, 1.937984496*m, 2.150537634*m,
- 2.277904328*m, 2.43902439*m, 2.941176471*m, 3.217503218*m, 3.429355281*m,
- 3.629764065*m, 3.78214826*m, 4.496402878*m, 7.4019245*m, 11.16071429*m, 14.38848921*m,
- 16.15508885*m, 17.69911504*m, 21.09704641*m, 23.04147465*m, 24.44987775*m,
- 30.76923077*m, 49.01960784*m,66.66666667*m,78.74015748*m,94.33962264*m,
- 102.145046*m, 108.4598698*m, 157.480315*m, 202.020202*m, 220.2643172*m,
- 211.4164905*m, 150.8295626*m, 117.5088132*m, 87.95074758*m,
- 60.60606061*m,18.11594203*m,11.79245283*m,9.652509653*m};
+    assert(sizeof(scintilD) == sizeof(photonEnergyScintillation));
 
-assert(sizeof(absorption) == sizeof(photonEnergy2));
+    G4MaterialPropertiesTable* waterMPT = new G4MaterialPropertiesTable();
 
-G4double scintilFast[] =
-{ 1.00, 1.00, 1.00, 1.00, 1.00, 1.00, 1.00,
-  1.00, 1.00, 1.00, 1.00, 1.00, 1.00, 1.00,
-  1.00, 1.00, 1.00, 1.00, 1.00, 1.00, 1.00,
-  1.00, 1.00, 1.00, 1.00, 1.00, 1.00, 1.00,
-  1.00, 1.00, 1.00, 1.00 };
+    waterMPT->AddProperty("RINDEX", photonEnergyIOF, refractiveIndex1, nEntriesIOF)->SetSpline(true);
+    waterMPT->AddProperty("ABSLENGTH",photonEnergyAbs, absorption, nEntriesAbs)->SetSpline(true);
+    // Data from Onken et al "Time response of water-based liquid scintillator from X-ray excitation"
+    // Assumption made that fast and slow components follow same distribution
+    waterMPT->AddProperty("FASTCOMPONENT",photonEnergyScintillation, scintilD, nEntriesScint)->SetSpline(true); // Energy Distribution for prompt scintillation
+    waterMPT->AddProperty("SLOWCOMPONENT",photonEnergyScintillation, scintilD, nEntriesScint)->SetSpline(true); // Energy distribution for decay scintillation
 
-assert(sizeof(scintilFast) == sizeof(photonEnergy));
+    // Data from Caravaca et al. "Characterization of water-based liquid scintillator for Cherenkov and scintillation separation"
+    waterMPT->AddConstProperty("SCINTILLATIONYIELD",234.0/MeV); // Light Yield Constant Number of photons/MeV
+    waterMPT->AddConstProperty("RESOLUTIONSCALE",1.0); // the normal distribution will follow a normal standard deviation width
+    waterMPT->AddConstProperty("FASTTIMECONSTANT", 2.25*ns); // value of a 1% WbLS
+    waterMPT->AddConstProperty("SLOWTIMECONSTANT",15.10*ns); // value of a 1% WbLS
+    waterMPT->AddConstProperty("YIELDRATIO",0.96); // Ratio of fast/slow(delay) photons
 
-G4double scintilSlow[] =
-{ 0.01, 1.00, 2.00, 3.00, 4.00, 5.00, 6.00,
-  7.00, 8.00, 9.00, 8.00, 7.00, 6.00, 4.00,
-  3.00, 2.00, 1.00, 0.01, 1.00, 2.00, 3.00,
-  4.00, 5.00, 6.00, 7.00, 8.00, 9.00, 8.00,
-  7.00, 6.00, 5.00, 4.00 };
-
-assert(sizeof(scintilSlow) == sizeof(photonEnergy));
-
-G4MaterialPropertiesTable* waterMPT = new G4MaterialPropertiesTable();
-
-waterMPT->AddProperty("RINDEX", photonEnergy, refractiveIndex1,nEntries)->SetSpline(true);
-waterMPT->AddProperty("ABSLENGTH",photonEnergy2, absorption, nEntries)->SetSpline(true);
-waterMPT->AddProperty("FASTCOMPONENT",photonEnergy, scintilFast, nEntries)->SetSpline(true);
-waterMPT->AddProperty("SLOWCOMPONENT",photonEnergy, scintilSlow, nEntries)->SetSpline(true);
-
-waterMPT->AddConstProperty("SCINTILLATIONYIELD",50./MeV);
-waterMPT->AddConstProperty("RESOLUTIONSCALE",1.0);
-waterMPT->AddConstProperty("FASTTIMECONSTANT", 1.*ns);
-waterMPT->AddConstProperty("SLOWTIMECONSTANT",10.*ns);
-waterMPT->AddConstProperty("YIELDRATIO",0.8);
-
-G4double energy_water[] = {
+    G4double energy_water[] = {
         1.56962*eV, 1.58974*eV, 1.61039*eV, 1.63157*eV,
         1.65333*eV, 1.67567*eV, 1.69863*eV, 1.72222*eV,
         1.74647*eV, 1.77142*eV, 1.7971 *eV, 1.82352*eV,
@@ -640,46 +640,31 @@ G4double energy_water[] = {
         3.99999*eV, 4.13332*eV, 4.27585*eV, 4.42856*eV,
         4.59258*eV, 4.76922*eV, 4.95999*eV, 5.16665*eV,
         5.39129*eV, 5.63635*eV, 5.90475*eV, 6.19998*eV
-};
+    };
 
   const G4int numentries_water = sizeof(energy_water)/sizeof(G4double);
 
-  G4double energy_ray_water[] = {
-          1.56962*eV, 1.58974*eV, 1.61039*eV, 1.63157*eV,
-          1.65333*eV, 1.67567*eV, 1.69863*eV, 1.72222*eV,
-          1.74647*eV, 1.77142*eV, 1.7971 *eV, 1.82352*eV,
-          1.85074*eV, 1.87878*eV, 1.90769*eV, 1.93749*eV,
-          1.96825*eV, 1.99999*eV, 2.03278*eV, 2.06666*eV,
-          2.10169*eV, 2.13793*eV, 2.17543*eV, 2.21428*eV,
-          2.25454*eV, 2.29629*eV, 2.33962*eV, 2.38461*eV,
-          2.43137*eV, 2.47999*eV, 2.53061*eV, 2.58333*eV
-  }; // 32 entries
-
-
-
-  const G4int numentries_water_ray = sizeof(energy_ray_water)/sizeof(G4double);
-
   G4double mie_water[] = {
-          167024.4*m, 158726.7*m, 150742  *m,
-          143062.5*m, 135680.2*m, 128587.4*m,
-          121776.3*m, 115239.5*m, 108969.5*m,
-          102958.8*m, 97200.35*m, 91686.86*m,
-          86411.33*m, 81366.79*m, 76546.42*m,
-          71943.46*m, 67551.29*m, 63363.36*m,
-          59373.25*m, 55574.61*m, 51961.24*m,
-          48527.00*m, 45265.87*m, 42171.94*m,
-          39239.39*m, 36462.50*m, 33835.68*m,
-          31353.41*m, 29010.30*m, 26801.03*m,
-          24720.42*m, 22763.36*m, 20924.88*m,
-          19200.07*m, 17584.16*m, 16072.45*m,
-          14660.38*m, 13343.46*m, 12117.33*m,
-          10977.70*m, 9920.416*m, 8941.407*m,
-          8036.711*m, 7202.470*m, 6434.927*m,
-          5730.429*m, 5085.425*m, 4496.467*m,
-          3960.210*m, 3473.413*m, 3032.937*m,
-          2635.746*m, 2278.907*m, 1959.588*m,
-          1675.064*m, 1422.710*m, 1200.004*m,
-          1004.528*m, 833.9666*m, 686.1063*m
+      167024.4*m, 158726.7*m, 150742  *m,
+      143062.5*m, 135680.2*m, 128587.4*m,
+      121776.3*m, 115239.5*m, 108969.5*m,
+      102958.8*m, 97200.35*m, 91686.86*m,
+      86411.33*m, 81366.79*m, 76546.42*m,
+      71943.46*m, 67551.29*m, 63363.36*m,
+      59373.25*m, 55574.61*m, 51961.24*m,
+      48527.00*m, 45265.87*m, 42171.94*m,
+      39239.39*m, 36462.50*m, 33835.68*m,
+      31353.41*m, 29010.30*m, 26801.03*m,
+      24720.42*m, 22763.36*m, 20924.88*m,
+      19200.07*m, 17584.16*m, 16072.45*m,
+      14660.38*m, 13343.46*m, 12117.33*m,
+      10977.70*m, 9920.416*m, 8941.407*m,
+      8036.711*m, 7202.470*m, 6434.927*m,
+      5730.429*m, 5085.425*m, 4496.467*m,
+      3960.210*m, 3473.413*m, 3032.937*m,
+      2635.746*m, 2278.907*m, 1959.588*m,
+      1675.064*m, 1422.710*m, 1200.004*m,
+      1004.528*m, 833.9666*m, 686.1063*m
   };
 
   assert(sizeof(mie_water) == sizeof(energy_water));
@@ -691,21 +676,9 @@ G4double energy_water[] = {
   waterMPT->AddConstProperty("MIEHG_BACKWARD",mie_water_const[1]);
   waterMPT->AddConstProperty("MIEHG_FORWARD_RATIO",mie_water_const[2]);
 
-  G4double rayleigh_water[] = {
-          2335.5*m, 2210.5*m, 2090.8*m, 1976.4*m, 1866.8*m, 1762.1*m,
-          1662.1*m, 1566.6*m, 1475.4*m, 1388.5*m, 1305.6*m, 1224.6*m,
-          1146.8*m, 1075.6*m, 1007.8*m, 941.23*m, 880.95*m, 821.12*m,
-          766.36*m, 714.50*m, 665.40*m, 618.97*m, 575.11*m, 533.69*m,
-          493.46*m, 457.10*m, 421.51*m, 389.06*m, 358.57*m, 329.98*m,
-          303.17*m, 277.43*m
-  }; // 32 entries
-
-// Note to Self: Never have spline set to true if rayleigh water goes to zero. Creates seg fault
-  assert(sizeof(rayleigh_water) == sizeof(energy_ray_water));
-  waterMPT->AddProperty("RAYLEIGH",energy_ray_water,rayleigh_water,numentries_water_ray)->SetSpline(true);
   Water->SetMaterialPropertiesTable(waterMPT);
-// Set the Birks Constant for the Water scintillator
-  Water->GetIonisation()->SetBirksConstant(0.126*mm/MeV);
+// Set the Birks Constant for the Water scintillator only works when using sensitive detector
+//Water->GetIonisation()->SetBirksConstant(0.126*mm/MeV);
 
 // ********************** APPLY OPTICAL QUALITIES ********************** //
 

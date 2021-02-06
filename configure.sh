eaaa03d8eaf6d11974ede5bd1ccd642abb809e5d
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-02-06 08:06:48 -0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -36,14 +36,15 @@ fi
 echo Configuring mantis...
 current_dir="$(pwd)"
 cd ~ 
-home_dir="$(pwd)"
-mkdir MANTIS_MAIN_DIR && mv $current_dir MANTIS_MAIN_DIR && cd MANTIS_MAIN_DIR/mantis
+bash_file=".bashrc"
+old_bash_file=".old_bashrc"
+
+mkdir MANTIS_MAIN_DIR && mv $current_dir MANTIS_MAIN_DIR && cp $bash_file MANTIS_MAIN_DIR/mantis cd MANTIS_MAIN_DIR/mantis && cp $bash_file $old_bash_file
 tar xfz NRF_Database.tar.gz && mv Database1.1 ../ && cd ../Database1.1
 database_working_dir="$(pwd)"
 echo "Exporting the Database working directory path: $database_working_dir"
-bash_file=$home_dir+"/.bashrc"
-echo $bash_file
-echo "export $database_working_dir" >> bash_file
+
+echo "export $database_working_dir" | tee -a $bash_file >/dev/null
 export G4NRFGAMMADATA=$database_working_dir
 
 if [ $ROOT_DIRECTORY != "None" ]
@@ -61,19 +62,22 @@ fi
 
 echo "Sourcing $VARROOTBIN"
 source $VARROOTBIN
-echo "source $VARROOTBIN" >> bash_file
+echo "source $VARROOTBIN" | tee -a $bash_file >/dev/null
 
 VAR4="/bin/geant4.sh"
 VARGEANTBIN=$GEANT4_DIR$VAR4
 echo "Sourcing $VARGEANTBIN"
 source $VARGEANTBIN
-echo "source $VARGEANTBIN" >> bash_file
+echo "source $VARGEANTBIN" | tee -a $bash_file >/dev/null
+
 VAR5="/share/Geant4-10.5.1/geant4make/geant4make.sh"
 VARGEANTMAKE=$GEANT4_DIR$VAR5
 echo "Sourcing $VARGEANTMAKE"
 source $VARGEANTMAKE
-echo "source $VARGEANTMAKE" >> bash_file
+echo "source $VARGEANTMAKE" | tee -a $bash_file >/dev/null
 
+cp $bash_file ~
+echo "Old bash file saved in: $(pwd)"
 echo Building Mantis...
 
 cd ../ && mkdir mantis_run && cd mantis_run && cmake ../mantis && make -j4 && cd ../mantis/Input_Files

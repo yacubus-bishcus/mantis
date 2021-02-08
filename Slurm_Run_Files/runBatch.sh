98a004d1e99689c8106a5795e035801d59c666d5
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-02-08 15:55:18 -0500

    Update runBatch.sh

--- Slurm_Run_Files/runBatch.sh
+++ Slurm_Run_Files/runBatch.sh
@@ -2,11 +2,12 @@
 
 START=$1
 LIMIT=$2
-export OUTFILE=$3
+INFILE=$3
+export OUTFILE=$4
 for ((a=START; a<LIMIT;a++))
 do
   export envvar_bsh=$a
-  sbatch submit_geant4.slurm &
+  sbatch submit_geant4.slurm $3 &
   sleep 0.2
 done 
 wait 

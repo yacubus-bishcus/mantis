7b27e055e222c1dfbeefa62618c65a9172fcffda
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-02-10 09:51:50 -0500

    Update HistoManager.cc

--- src/HistoManager.cc
+++ src/HistoManager.cc
@@ -36,7 +36,7 @@ void HistoManager::Book(G4bool bremTest)
   G4AnalysisManager* manager = G4AnalysisManager::Instance();
   G4int nbins = 100000;
   manager->SetVerboseLevel(0);
-  #ifdef G4_OPTPARAM
+  #ifndef G4_OPTPARAM
     manager->SetNtupleMerging(true);
   #endif
   xmax = chosen_energy;

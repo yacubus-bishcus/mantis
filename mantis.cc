c34f6d9fd2ee9cdc20b922d0fca9d70109c8f854
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-02-10 18:52:55 -0500

    Update mantis.cc

--- mantis.cc
+++ mantis.cc
@@ -194,8 +194,8 @@ int main(int argc,char **argv)
 #endif
 
         G4int stop_time = time(0);
-        G4cout << G4endl << "*-----------------------------------------------*" << G4endl;
-        G4cout << G4endl << "*-----------------------------------------------*" << G4endl;
+        G4cout << G4endl << "*-----------------------------------------------------------*" << G4endl;
+        G4cout << G4endl << "*-----------------------------------------------------------*" << G4endl;
         G4cout << G4endl << "The MC took:\t\t" << stop_time - start_time << "s" << G4endl << G4endl;
 
         delete LoggedSession;

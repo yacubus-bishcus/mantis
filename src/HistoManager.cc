c01840ed7181d2d788db1a734d81b5538e0e8fb5
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-02-17 18:39:29 -0500

    Update HistoManager.cc

--- src/HistoManager.cc
+++ src/HistoManager.cc
@@ -83,7 +83,7 @@ void HistoManager::Book()
                 return;
         }
 
-        G4int n_bins = xmax/(10.0e-6/2.0);
+        G4int nbins = xmax/(10.0e-6/2.0);
         // Create ID 0 Ntuple for Incident Chopper Data
         manager->CreateNtuple("ChopIn", "Chopper Wheel Incident Data");
         manager->CreateNtupleDColumn("Energy");

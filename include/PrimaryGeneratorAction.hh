46a4d39f85d486efc4603735ca8fdbf5cac1c6b4
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-02-18 10:47:23 -0500

    Update PrimaryGeneratorAction.hh

--- include/PrimaryGeneratorAction.hh
+++ include/PrimaryGeneratorAction.hh
@@ -63,7 +63,7 @@ G4ParticleGun* GetParticleGun()
 G4double SampleUResonances();
 
 private:
-G4double beamStart = 124.0;
+G4double beamStart = 129.9;
 G4bool file_check;
 G4ParticleGun* fParticleGun;
 

a4ed830f01af70c7a8d2042ae92fb3c6ef196349
Jacob Bickus <j.bickus2019@gmail.com>
2021-02-24 23:38:51 -0500

    fancier

--- parallel.sh
+++ parallel.sh
@@ -10,10 +10,25 @@
 #// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #///////////////////////////////////////////////////////////////////////////////
 
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        # linux
+    CORES=$(grep -c ^processor /proc/cpuinfo)
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+    CORES=$(sysctl -a | grep core_count | sed 's/machdep.cpu.core_count: //')
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        # ...
+    CORES=$(sysctl -a | grep core_count | sed 's/machdep.cpu.core_count: //')
+else
+     echo 'Unknown OS';
+     CORES=-1;
+fi
+
 cd ../
 mkdir RunMantis
 cd RunMantis
 cp ../mantis/mantis.cc ../mantis/mantis_mpi.cc
 mkdir tmp
 cmake -DWITH_MPI=ON -DGeant4_DIR=/home/jbickus/geant4/lib[64]/Geant4-10.5.1 -DG4mpi_DIR=/home/jbickus/GEANT4MPI/install/lib64/G4mpi-10.5.1 ../mantis
-make -j4
+echo "Compiling with {$CORES} cores..."
+make -j$CORES

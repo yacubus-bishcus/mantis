f64a6dbedbdc38b92ae7ef6e2111b8beda9ed2dd
Jacob Bickus <j.bickus2019@gmail.com>
2021-02-24 23:35:47 -0500

    done

--- parallel.sh
+++ parallel.sh
@@ -10,6 +10,9 @@
 #// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #///////////////////////////////////////////////////////////////////////////////
 
+cd ../
+mkdir RunMantis
+cd RunMantis
 cp ../mantis/mantis.cc ../mantis/mantis_mpi.cc
 mkdir tmp
 cmake -DWITH_MPI=ON -DGeant4_DIR=/home/jbickus/geant4/lib[64]/Geant4-10.5.1 -DG4mpi_DIR=/home/jbickus/GEANT4MPI/install/lib64/G4mpi-10.5.1 ../mantis

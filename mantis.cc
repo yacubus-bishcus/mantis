e8e5df807084be907f6b5030052dd1b5edfe7a96
Jacob Bickus <j.bickus2019@gmail.com>
2021-02-25 12:27:18 -0500

    try this

--- mantis.cc
+++ mantis.cc
@@ -155,7 +155,7 @@ int main(int argc,char** argv)
 
   #ifdef MANTIS_MPI_ENABLED
     std::cout << "Instatiating G4MPImanager with: " << argc << " arguments." << std::endl;
-    G4MPImanager* g4MPI= new G4MPImanager(argc, argv);
+    G4MPImanager* g4MPI= new G4MPImanager(argc, argv,1);
     G4MPIsession* session= g4MPI->GetMPIsession();
     std::cout << "Session Grabbed." << std::endl;
   #endif

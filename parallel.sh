553f77cbb9b2eb1ae6d0c5c9c649154ebbfcf004
Jacob Bickus <j.bickus2019@gmail.com>
2021-02-25 11:17:00 -0500

    done

--- parallel.sh
+++ parallel.sh
@@ -27,7 +27,7 @@ fi
 cd ../
 mkdir RunMantis
 cd RunMantis
-cp ../mantis/mantis.cc ../mantis/mantis_mpi.cc
+cp ../mantis/mantis.cc ../mantis/mpi_mantis.cc
 mkdir tmp
 cmake -DWITH_MPI=ON -DGeant4_DIR=/home/jbickus/geant4/lib64/Geant4-10.5.1 -DCMAKE_CXX_COMPILER=mpicxx -DG4mpi_DIR=/home/jbickus/GEANT4MPI/install/lib64/G4mpi-10.5.1 ../mantis
 echo "Compiling with $CORES cores..."

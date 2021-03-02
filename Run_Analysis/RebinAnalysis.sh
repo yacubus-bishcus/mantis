76433e541bbcc2f89cb76be3e00eeeef746386a2
Jacob Bickus <j.bickus2019@gmail.com>
2021-03-02 07:38:19 -0500

    added print results only option

--- Run_Analysis/RebinAnalysis.sh
+++ Run_Analysis/RebinAnalysis.sh
@@ -8,6 +8,7 @@ WATERDATA="false"
 DETDATA="true"
 INCDETDATA="false"
 PRINTDATA="false"
+PRINTDATAONLY="false"
 
 for arg in "$@"
 do
@@ -26,6 +27,7 @@ do
         echo "--DetData=true          specify Detected Data Analysis"
         echo "--IncDetData=false      specify Incident Detector Data Analysis"
         echo "--PrintData=false       Run PrintResults.cc"
+        echo "--PrintDataOnly=false   Only Run Print Results.cc Do not Rebin!"
         shift
         exit 0
         ;;
@@ -69,12 +71,15 @@ do
        PRINTDATA="${arg#*=}"
        shift
        ;;
+       -PrintOnly|--PrintDataOnly=*)
+       PRINTDATAONLY="${arg#*=}"
+       shift
+       ;;
     esac
 done
 
 if [ $INFILE != "DEFAULT" ]
 then
-  echo "Rebinning File: $INFILE"
   echo "Chopper Data Set to: $CHOPDATA"
   echo "Interrogation Object Data: $INTOBJDATA"
   echo "NRF Data: $NRFDATA"
@@ -83,97 +88,107 @@ then
   echo "Detected Data: $DETDATA"
   echo "Incident Detector Data: $INCDETDATA"
   echo "Print Results: $PRINTDATA"
+  echo "Print Results Only: $PRINTDATAONLY"
+
+  if [ $PRINTDATAONLY == "false" ]
+  then
+    echo "Rebinning File: $INFILE"
+  fi
 else
   echo "USER ERROR: MUST INPUT File to Rebin!"
   exit
 fi
 
 
-if [ $INFILE2 != "NA" ]
+if [ $INFILE2 != "NA" ] && [ $PRINTDATAONLY == "false" ]
 then
   echo "Rebinning Off File: $INFILE2"
 fi
 
-VAR1="\""
-
-theFile=$VAR1$INFILE$VAR1
-theFile2=$VAR1$INFILE2$VAR1
-
-if [ $CHOPDATA != "false" ] && [ $CHOPDATA != "False" ]
+if [ $PRINTDATAONLY == "false" ]
 then
-  root -l -b -q "Rebin.cc($theFile,\"ChopIn\",\"hChopIn\",300)"
-  root -l -b -q "Rebin.cc($theFile,\"ChopOut\",\"hChopOut\",300)"
-  if [ $INFILE2 != "NA" ]
+  VAR1="\""
+
+  theFile=$VAR1$INFILE$VAR1
+  theFile2=$VAR1$INFILE2$VAR1
+
+  if [ $CHOPDATA != "false" ] && [ $CHOPDATA != "False" ]
   then
-    root -l -b -q "Rebin.cc($theFile2,\"ChopIn\",\"hChopIn\",300)"
-    root -l -b -q "Rebin.cc($theFile2,\"ChopOut\",\"hChopOut\",300)"
+    root -l -b -q "Rebin.cc($theFile,\"ChopIn\",\"hChopIn\",300)"
+    root -l -b -q "Rebin.cc($theFile,\"ChopOut\",\"hChopOut\",300)"
+    if [ $INFILE2 != "NA" ]
+    then
+      root -l -b -q "Rebin.cc($theFile2,\"ChopIn\",\"hChopIn\",300)"
+      root -l -b -q "Rebin.cc($theFile2,\"ChopOut\",\"hChopOut\",300)"
+    fi
   fi
-fi
 
-if [ $INTOBJDATA != "false" ] && [ $INTOBJDATA != "False" ]
-then
-  # Rebin IntObj > 1.5 MeV with 600 bins bin width 1e-3
-  root -l -b -q "Rebin.cc($theFile,\"IntObjIn\",\"hIntObjIn\",600,1.5,2.1,\"Energy>1.5\")"
-  # Rebin IntObj > 1.5 MeV with 120,000 bins bin width 5e-6
-  root -l -b -q "Rebin.cc($theFile,\"IntObjIn\",\"hIntObjIn\",120000,1.5,2.1,\"Energy>1.5\")"
-  root -l -b -q "Rebin.cc($theFile,\"IntObjIn\",\"hIntObjIn\",300)"
-  root -l -b -q "Rebin.cc($theFile,\"IntObjOut\",\"hIntObjOut\",300)"
-  if [ $INFILE2 != "NA" ]
+  if [ $INTOBJDATA != "false" ] && [ $INTOBJDATA != "False" ]
   then
     # Rebin IntObj > 1.5 MeV with 600 bins bin width 1e-3
-    root -l -b -q "Rebin.cc($theFile2,\"IntObjIn\",\"hIntObjIn\",600,1.5,2.1,\"Energy>1.5\")"
+    root -l -b -q "Rebin.cc($theFile,\"IntObjIn\",\"hIntObjIn\",600,1.5,2.1,\"Energy>1.5\")"
     # Rebin IntObj > 1.5 MeV with 120,000 bins bin width 5e-6
-    root -l -b -q "Rebin.cc($theFile2,\"IntObjIn\",\"hIntObjIn\",120000,1.5,2.1,\"Energy>1.5\")"
-    root -l -b -q "Rebin.cc($theFile2,\"IntObjIn\",\"hIntObjIn\",300)"
-    root -l -b -q "Rebin.cc($theFile2,\"IntObjOut\",\"hIntObjOut\",300)"
+    root -l -b -q "Rebin.cc($theFile,\"IntObjIn\",\"hIntObjIn\",120000,1.5,2.1,\"Energy>1.5\")"
+    root -l -b -q "Rebin.cc($theFile,\"IntObjIn\",\"hIntObjIn\",300)"
+    root -l -b -q "Rebin.cc($theFile,\"IntObjOut\",\"hIntObjOut\",300)"
+    if [ $INFILE2 != "NA" ]
+    then
+      # Rebin IntObj > 1.5 MeV with 600 bins bin width 1e-3
+      root -l -b -q "Rebin.cc($theFile2,\"IntObjIn\",\"hIntObjIn\",600,1.5,2.1,\"Energy>1.5\")"
+      # Rebin IntObj > 1.5 MeV with 120,000 bins bin width 5e-6
+      root -l -b -q "Rebin.cc($theFile2,\"IntObjIn\",\"hIntObjIn\",120000,1.5,2.1,\"Energy>1.5\")"
+      root -l -b -q "Rebin.cc($theFile2,\"IntObjIn\",\"hIntObjIn\",300)"
+      root -l -b -q "Rebin.cc($theFile2,\"IntObjOut\",\"hIntObjOut\",300)"
+    fi
   fi
-fi
 
-if [ $NRFDATA != "false" ] && [ $NRFDATA != "False" ]
-then
-  root -l -b -q "Rebin.cc($theFile,\"NRF\",\"hNRF\",300)"
-  if [ $INFILE2 != "NA" ]
+  if [ $NRFDATA != "false" ] && [ $NRFDATA != "False" ]
   then
-    root -l -b -q "Rebin.cc($theFile2,\"NRF\",\"hNRF\",300)"
+    root -l -b -q "Rebin.cc($theFile,\"NRF\",\"hNRF\",300)"
+    if [ $INFILE2 != "NA" ]
+    then
+      root -l -b -q "Rebin.cc($theFile2,\"NRF\",\"hNRF\",300)"
+    fi
   fi
-fi
 
-if [ $WATERDATA != "false" ] && [ $WATERDATA != "False" ]
-then
-  root -l -b -q "Rebin.cc($theFile,\"Water\",\"hWater\",300)"
-  if [ $INFILE2 != "NA" ]
+  if [ $WATERDATA != "false" ] && [ $WATERDATA != "False" ]
   then
-    root -b -q "Rebin.cc($theFile2,\"Water\",\"hWater\",300)"
+    root -l -b -q "Rebin.cc($theFile,\"Water\",\"hWater\",300)"
+    if [ $INFILE2 != "NA" ]
+    then
+      root -b -q "Rebin.cc($theFile2,\"Water\",\"hWater\",300)"
+    fi
   fi
-fi
 
-if [ $CHERENKOVDATA != "false" ] && [ $CHERENKOVDATA != "False" ]
-then
-  root -l -b -q "Rebin.cc($theFile,\"Cherenkov\",\"hCherenkov\",300)"
-  if [ $INFILE2 != "NA" ]
+  if [ $CHERENKOVDATA != "false" ] && [ $CHERENKOVDATA != "False" ]
   then
-    root -l -b -q "Rebin.cc($theFile2,\"Cherenkov\",\"hCherenkov\",300)"
+    root -l -b -q "Rebin.cc($theFile,\"Cherenkov\",\"hCherenkov\",300)"
+    if [ $INFILE2 != "NA" ]
+    then
+      root -l -b -q "Rebin.cc($theFile2,\"Cherenkov\",\"hCherenkov\",300)"
+    fi
   fi
-fi
 
-if [ $DETDATA != "false" ] && [ $DETDATA != "False" ]
-then
-  # Rebin Detected with 20 bins Energy < 5e-6 bin width 0.25e-6
-  root -l -b -q "Rebin.cc($theFile,\"DetInfo\",\"hDet\",20,0.0,5e-6,\"Energy<5e-6\")"
-  if [ $INFILE2 != "NA" ]
+  if [ $DETDATA != "false" ] && [ $DETDATA != "False" ]
   then
-    root -l -b -q "Rebin.cc($theFile2,\"DetInfo\",\"hDet\",20,0.0,5e-6,\"Energy<5e-6\")"
+    # Rebin Detected with 20 bins Energy < 5e-6 bin width 0.25e-6
+    root -l -b -q "Rebin.cc($theFile,\"DetInfo\",\"hDet\",20,0.0,5e-6,\"Energy<5e-6\")"
+    if [ $INFILE2 != "NA" ]
+    then
+      root -l -b -q "Rebin.cc($theFile2,\"DetInfo\",\"hDet\",20,0.0,5e-6,\"Energy<5e-6\")"
+    fi
   fi
-fi
 
-if [ $INCDETDATA != "false" ] && [ $INCDETDATA != "False" ]
-then
-  root -l -b -q "Rebin.cc($theFile,\"IncDetInfo\",\"hIncDetInfo\",300)"
-  if [ $INFILE2 != "NA" ]
+  if [ $INCDETDATA != "false" ] && [ $INCDETDATA != "False" ]
   then
-    root -l -b -q "Rebin.cc($theFile2,\"IncDetInfo\",\"hIncDetInfo\",300)"
+    root -l -b -q "Rebin.cc($theFile,\"IncDetInfo\",\"hIncDetInfo\",300)"
+    if [ $INFILE2 != "NA" ]
+    then
+      root -l -b -q "Rebin.cc($theFile2,\"IncDetInfo\",\"hIncDetInfo\",300)"
+    fi
   fi
-fi
+
+fi 
 
 if [ $INFILE2 != "NA" ] && [ $PRINTDATA != "false" ]
 then

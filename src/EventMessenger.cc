02e35a398fd466c55c7a0290e24867e9c809e822
Jacob Bickus <j.bickus2019@gmail.com>
2021-03-04 08:53:17 -0500

    bug

--- src/EventMessenger.cc
+++ src/EventMessenger.cc
@@ -9,7 +9,6 @@ EventMessenger::EventMessenger(EventAction* EventAction)
   Cmd = new G4UIcmdWithADouble("/mantisevent/eventInfoFreq",this);
   Cmd->SetGuidance("Choose Desired eventInfoFreq");
   Cmd->SetParameterName("eventInfo",false);
-  Cmd->SetDefaultValue("100000");
   Cmd->SetRange("eventInfo > 0 && eventInfo < 100000000");
 }
 

ceb70f771deb77525381d49740e82c3f039ad2c9
Jacob Bickus <j.bickus2019@gmail.com>
2021-03-05 14:54:00 -0500

    complete

--- Run_Analysis/SNR.cc
+++ Run_Analysis/SNR.cc
@@ -34,33 +34,93 @@ void SNR(const char* inFile)
     double outMax = aIntObjOut->GetMaximum("Energy");
     aIntObjIn->SetEstimate(-1);
     aIntObjOut->SetEstimate(-1);
-    TH1D* e1 = new TH1D("e1","IntObjIn NRF Histogram",100,1.73350, 1.73360);
-    TH1D* e2 = new TH1D("e2","IntObjOut NRF Histogram",100,1.73350,1.73360);
+
+    TH1D* e1 = new TH1D("e11","IntObjIn NRF Histogram",100,1.73350, 1.73360);
+    TH1D* e11 = new TH1D("e10","IntObjIn NRF Histogram",100,1.65620,1.65630);
+    TH1D* e12 = new TH1D("e12","IntObjIn NRF Histogram",100,1.81520,1.81530);
+    TH1D* e13 = new TH1D("e13","IntObjIn NRF Histogram",100,1.86230,1.86240);
+    TH1D* e14 = new TH1D("e14","IntObjIn NRF Histogram",100,2.00610,2.00620);
+
+    TH1D* e2 = new TH1D("e21","IntObjOut NRF Histogram",100,1.73350,1.73360);
+    TH1D* e11 = new TH1D("e20","IntObjOut NRF Histogram",100,1.65620,1.65630);
+    TH1D* e12 = new TH1D("e22","IntObjOut NRF Histogram",100,1.81520,1.81530);
+    TH1D* e13 = new TH1D("e23","IntObjOut NRF Histogram",100,1.86230,1.86240);
+    TH1D* e14 = new TH1D("e24","IntObjOut NRF Histogram",100,2.00610,2.00620);
+
     TH1D* eT = new TH1D("eT","IntObjIn Histogram",100,0.0,inMax);
     TH1D* eT2 = new TH1D("eT2","IntObjOut Histogram",100,0.0,outMax);
-    aIntObjIn->Draw("Energy>>e1","Weight","goff");
-    aIntObjOut->Draw("Energy>>e2","Weight","goff");
+
+    std::cout << "Drawing IntObjIn NRF Histograms..." << std::endl;
+    aIntObjIn->Draw("Energy>>e10","Weight","goff");
+    aIntObjIn->Draw("Energy>>e11","Weight","goff");
+    aIntObjIn->Draw("Energy>>e12","Weight","goff");
+    aIntObjIn->Draw("Energy>>e13","Weight","goff");
+    aIntObjIn->Draw("Energy>>e14","Weight","goff");
+
+    std::cout << "Drawing IntObjOut NRF Histograms..." << std::endl;
+    aIntObjOut->Draw("Energy>>e20","Weight","goff");
+    aIntObjOut->Draw("Energy>>e21","Weight","goff");
+    aIntObjOut->Draw("Energy>>e22","Weight","goff");
+    aIntObjOut->Draw("Energy>>e23","Weight","goff");
+    aIntObjOut->Draw("Energy>>e24","Weight","goff");
+
+    std::cout << "Drawing IntObjIn and IntObjOut Histograms..." << std::endl;
     aIntObjIn->Draw("Energy>>eT","Weight","goff");
     aIntObjOut->Draw("Energy>>eT2","Weight","goff");
-    e1->Print();
-    eT->Print();
-    e2->Print();
-    eT2->Print();
-    int binStart = e1->GetXaxis()->FindBin(1.73354);
-    int binEnd = e1->GetXaxis()->FindBin(1.73355);
-    double inSignal = e1->Integral(binStart, binEnd);
-    std::cout << "IntObjIn Signal: \t" << inSignal << std::endl;
+    double eStart[] = {1.65623,1.73354,1.81525,1.86231,2.00619};
+    double eEnd[5];
+
+    for(int i=0;i<5;++i)
+    {
+      eEnd[i] = eStart[i] + 0.00001;
+    }
+
+    int binStart = e10->GetXaxis()->FindBin(eStart[0]);
+    int binEnd = e10->GetXaxis()->FindBin(eEnd[0]);
+    double inSignal[5];
+    double outSignal[5];
+    inSignal[0] = e10->Integral(binStart, binEnd);
+    outSignal[0] = e20->Integral(binStart,binEnd);
+    binStart = e11->GetXaxis()->FindBin(eStart[1]);
+    binEnd = e11->GetXaxis()->FindBin(eEnd[1]);
+    inSignal[1] = e11->Integral(binStart,binEnd);
+    outSignal[1] = e21->Integral(binStart,binEnd);
+    binStart = e12->GetXaxis()->FindBin(eStart[2]);
+    binEnd = e12->GetXaxis()->FindBin(eEnd[2]);
+    inSignal[2] = e12->Integral(binStart,binEnd);
+    outSignal[2] = e22->Integral(binStart,binEnd);
+    binStart = e13->GetXaxis()->FindBin(eStart[3]);
+    binEnd = e13->GetXaxis()->FindBin(eEnd[3]);
+    inSignal[3] = e13->Integral(binStart,binEnd);
+    outSignal[3] = e23->Integral(binStart,binEnd);
+    binStart = e14->GetXaxis()->FindBin(eStart[4]);
+    binEnd = e14->GetXaxis()->FindBin(eEnd[4]);
+    inSignal[4] = e14->Integral(binStart,binEnd);
+    outSignal[4] = e24->Integral(binStart,binEnd);
+
+    double tSignalin = 0;
+    double tSignalout = 0;
+
+    for(int i=0;i<5;++i)
+    {
+      std::cout << "IntObjIn Signals: \t" << inSignal[i] << std::endl;
+      tSignalin += inSignal[i];
+      std::cout << "IntObjOut Signals: \t" << outSignal[i] << std::endl;
+      tSignalout += outSignal[i];
+    }
+    std::cout << "Total IntObjIn Signal: \t" << tSignalin << std::endl;
+    std::cout << "Total IntObjOut Signal: \t" << tSignalout << std::endl;
+
     double inNoise = eT->Integral();
     std::cout << "IntObjIn Noise: \t" << inNoise << std::endl;
-    double outSignal = e2->Integral(binStart,binEnd);
-    std::cout << "IntObjOut Signal: \t" << outSignal << std::endl;
     double outNoise = eT2->Integral();
     std::cout << "IntObjOut Noise: \t" << outNoise << std::endl << std::endl;
+
     std::cout << "The NRF photons are removed from the beam in the chopper wheel stage."
               << std::endl
               << "A lower SNR is better because you want the signal from NRF to be minimized."
               << std::endl;
-    std::cout << "IntObjIn SNR: \t" << abs(inSignal - inNoise)/sqrt(inNoise) << std::endl;
-    std::cout << "IntObjOut SNR: \t" << abs(outSignal - outNoise)/sqrt(outNoise) << std::endl;
+    std::cout << "IntObjIn SNR: \t" << abs(tSignalin - inNoise)/sqrt(inNoise) << std::endl;
+    std::cout << "IntObjOut SNR: \t" << abs(tSignalout - outNoise)/sqrt(outNoise) << std::endl;
   }
 }

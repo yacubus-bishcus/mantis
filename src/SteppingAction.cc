79237ad246ceb10c539a4561f7d8f195dfe7e56e
Jacob Bickus <j.bickus2019@gmail.com>
2021-03-28 14:49:51 -0400

    changed time cut to be shorter

--- src/SteppingAction.cc
+++ src/SteppingAction.cc
@@ -102,7 +102,7 @@ void SteppingAction::UserSteppingAction(const G4Step* aStep)
     }
 
     // Run Time Cut
-    if(theTrack->GetGlobalTime() > 1500) // cut placed on particles time greater than 1500 ns
+    if(theTrack->GetGlobalTime() > 250) // cut placed on particles time greater than 1500 ns
     {
       theTrack->SetTrackStatus(fStopAndKill);
       krun->AddStatusKilledTime();
